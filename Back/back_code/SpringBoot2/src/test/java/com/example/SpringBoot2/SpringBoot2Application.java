dd6fa81566eea248397d6ad5b367d658bbf1a4f4
Raske <125515277+R4skex@users.noreply.github.com>
2024-10-25 17:28:05 -0300

    Add files via upload

---
+++ Back/back_code/SpringBoot2/src/test/java/com/example/SpringBoot2/SpringBoot2Application.java
@@ -0,0 +1,14 @@
+package com.example.SpringBoot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2Application.class, args);
+	}
+
+
+}
